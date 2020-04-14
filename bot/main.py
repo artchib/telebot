f25f17c18b1c6df8939ed4de5409291127fca4a7
artchib <strelcov_a@yahoo.com>
2020-04-14 12:56:33 +0700

    Доделан калькулятор с парой BTC-USD

--- bot/main.py
+++ bot/main.py
@@ -6,6 +6,9 @@ from telegram.ext import MessageHandler
 from telegram.ext import CallbackQueryHandler
 from telegram import InlineKeyboardMarkup
 from telegram import InlineKeyboardButton
+from telegram.ext import Filters
+# from telegram import KeyboardButton
+# from telegram import ReplyKeyboardMarkup
 from bot.bittrex import BittrexClient
 from bot.bittrex import BittrexError
 
@@ -13,6 +16,18 @@ from bot.config import TG_TOKEN
 
 USD_BTC_PAIR = 'USD-BTC'
 USD_DASH_PAIR = 'USD-DASH'
+BTC_TO_USD_BUTT = 'BTC--->USD'
+USD_TO_BTC_BUTT = 'USD--->BTC'
+
+# def get_convert_keyboard():
+#     keyboard = [
+#         [
+#             KeyboardButton(BTC_TO_USD_BUTT),
+#             KeyboardButton(USD_TO_BTC_BUTT),
+#
+#         ],
+#     ]
+#     return ReplyKeyboardMarkup(keyboard=keyboard, resize_keyboard=True)
 
 def back_button():
 
@@ -25,10 +40,10 @@ def back_button():
 def get_main_keyboard():
     keyboard = [
         [
-            InlineKeyboardButton(text='Курсы криптовалют', callback_data='rates')
+            InlineKeyboardButton(text=BTC_TO_USD_BUTT, callback_data='btc_to_usd')
          ],
         [
-            InlineKeyboardButton(text='Конвертер', callback_data='converter')
+            InlineKeyboardButton(text=USD_TO_BTC_BUTT, callback_data='usd_to_btc')
         ],
     ]
     return InlineKeyboardMarkup(keyboard)
@@ -37,17 +52,41 @@ def button_callback_handler(bot: Bot, update: Update):
     query = update.callback_query
     call_data = query.data
     print(call_data)
+    global cur_to_cur
 
-    if call_data == 'rates':
+    if call_data == 'btc_to_usd':
         client = BittrexClient()
+        global rate_btc
         rate_btc = client.get_last_price(pair=USD_BTC_PAIR)
-        rate_dash = client.get_last_price(pair=USD_DASH_PAIR)
-        text = f'Сейчас за один биткоин дают {rate_btc} долларов\n А за один DASH {rate_dash}'
+        text = f'Сейчас за один биткоин дают {rate_btc} долларов\n Введите кол-во BTC:'
         print(f'srabotal btc--usd ----- {rate_btc}')
+        cur_to_cur = 'BTC_USD'
         query.edit_message_text(
             text=text,
             reply_markup=back_button()
         )
+
+    if call_data == 'usd_to_btc':
+        client = BittrexClient()
+        rate_btc = client.get_last_price(pair=USD_BTC_PAIR)
+        text = f'Сейчас за один доллар дают {1/rate_btc} биткойнов\n Введите кол-во $:'
+        print(f'srabotal btc--usd ----- {rate_btc}')
+        cur_to_cur = 'USD_BTC'
+        query.edit_message_text(
+            text=text,
+            reply_markup=back_button()
+        )
+
+
+    if call_data == 'converter':
+        # print('RABOTAET KONVERTER')
+        # query.edit_message_text(
+        #     text='Это ковертер',
+        #     reply_markup=get_convert_keyboard(),
+        # )
+
+        pass
+
 
     if call_data == 'btmk':
         print('srabotal BACK')
@@ -65,7 +104,20 @@ def do_start(bot: Bot, update: Update):
     )
 
 def do_text(bot: Bot, update: Update):
-    pass
+    chat_id = update.message.chat_id
+    message = update.message.text
+    global cur_to_cur, rate_btc
+    if message.isdigit:
+        print(f'сообщение цифра, cur_to_cur={cur_to_cur}, цифра = {message}')
+        if cur_to_cur == 'BTC_USD':
+            text = f'= {float(message) * rate_btc} $'
+        elif cur_to_cur == 'USD_BTC':
+            text = f'= {float(message) / rate_btc} BTC'
+        bot.send_message(
+            chat_id=chat_id,
+            text=text,
+            reply_markup=get_main_keyboard(),
+        )
 
 
 def main():
@@ -75,12 +127,15 @@ def main():
     updater = Updater(
         bot=bot
     )
+    global cur_to_cur
 
     start_handler = CommandHandler('start', do_start)
     buttons_handler = CallbackQueryHandler(callback=button_callback_handler)
+    text_handler = MessageHandler(Filters.text, do_text)
 
     updater.dispatcher.add_handler(start_handler)
     updater.dispatcher.add_handler(buttons_handler)
+    updater.dispatcher.add_handler(text_handler)
 
     updater.start_polling()
     updater.idle()
