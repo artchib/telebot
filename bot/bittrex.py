5de29c72bd29fd00a52ebf7d087dae9727720909
artchib <strelcov_a@yahoo.com>
2020-04-29 21:14:07 +0700

    t_token has been deleted

--- bot/bittrex.py
+++ bot/bittrex.py
@@ -1,3 +1,4 @@
+# coding: utf8
 import requests
 from logging import getLogger
 
--- bot/main.py
+++ bot/main.py
@@ -1,3 +1,4 @@
+# coding: utf8
 from telegram import Bot
 from telegram import Update
 from telegram.ext import Updater
